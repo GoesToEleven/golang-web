beb4d2a2e1d11b5b718dee045edbaed2fc5e037a
GoesToEleven <tuddleymc@gmail.com>
2016-02-02 21:32:17 -0800

    solution to 018 challenge

---
+++ zzz_temp/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			go func(m, n int) {
+				fmt.Printf("%d %d\n", m, n)
+			}(i, j)
+		}
+	}
+	var a string
+	fmt.Scanln(&a)
+}
