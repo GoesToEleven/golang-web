bc5776e8920c933e4942e50af8176295fe925155
GoesToEleven <tuddleymc@gmail.com>
2016-04-17 18:33:49 -0700

    js examples complete

--- 053_javascript/03_js-curriculum/13/02_angular/builds/development/js/02_controller.js
+++
@@ -1,9 +0,0 @@
-var myApp = angular.module('myApp', []);
-
-myApp.controller('MyController', function MyController($scope){
-    $scope.author = {
-        name: 'Todd McLeod',
-        title: 'Staff Author',
-        company: 'Lynda'
-    }
-});
--- 053_javascript/03_js-curriculum/13/03_angular_json/builds/development/js/controller.js
+++
@@ -1,9 +0,0 @@
-var myApp = angular.module('myApp', []);
-
-myApp.controller('MyController', ['$scope', '$http', function($scope, $http){
-    $http.get('js/data.json').success(function(data) {
-        console.log(data);
-        $scope.artists = data.artists;
-        console.log($scope.artists);
-    });
-}]);
