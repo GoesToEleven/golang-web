e211aa690f0cb538304860339396508fd20b690e
GoesToEleven <tuddleymc@gmail.com>
2016-04-19 08:53:56 -0700

    entry checker challenge

--- 054_ajax/03_entry-checker_challenge/01_instructions/readme.txt
+++ 054_ajax/03_entry-checker_challenge/01_instructions/readme.txt
@@ -9,3 +9,9 @@ The words will be stored in either memcache or the datastore.
 If the word is already stored, a message will display on the webpage letting the user know that word is already stored.
 
 Begin with the code in this folder.
+
+Try to build the code yourself before looking at the solution.
+
+If you are stuck, look at previous examples.
+
+If you are completely stuck, read through the solution for understanding.
--- 054_ajax/03_entry-checker_challenge/02_solution/main.go
+++ 054_ajax/03_entry-checker_challenge/02_solution/main.go
@@ -3,15 +3,58 @@ package checker
 import (
 	"html/template"
 	"net/http"
+	"encoding/json"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/user"
+	"google.golang.org/appengine/datastore"
 )
+
+type Word struct {
+	Name string
+}
 
 var tpl *template.Template
 
 func init() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/api/check", wordCheck)
+	http.HandleFunc("/api/insert", wordInsert)
+
+	// serve public resources
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public"))))
+
+	// parse templates
 	tpl = template.Must(template.ParseGlob("*.html"))
-	http.Handle("/", index)
 }
 
 func index(res http.ResponseWriter, req *http.Request) {
 	tpl.ExecuteTemplate(res, "index.html", nil)
+}
+
+func wordCheck(res http.ResponseWriter, req *http.Request) {
+
+	ctx := appengine.NewContext(req)
+
+	var w Word
+	json.NewDecoder(req.Body).Decode(&w)
+	key := datastore.NewKey(ctx, "Dictionary", w, 0, nil)
+	err := datastore.Get(ctx, key, &w)
+	if err != nil {
+		json.NewEncoder(res).Encode("false")
+		return
+	}
+	json.NewEncoder(res).Encode("true")
+}
+
+func wordInsert(res http.ResponseWriter, req *http.Request) {
+	var profile Profile
+	json.NewDecoder(req.Body).Decode(&profile)
+
+	ctx := appengine.NewContext(req)
+	u := user.Current(ctx)
+	key := datastore.NewKey(ctx, "Profile", u.Email, 0, nil)
+	_, err := datastore.Put(ctx, key, &profile)
+	if err != nil {
+		http.Error(res, err.Error(), 500)
+	}
 }
